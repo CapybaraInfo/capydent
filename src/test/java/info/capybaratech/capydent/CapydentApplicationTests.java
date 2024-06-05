4713b0e2d8c4303e62a2c5df148b2564c48eb111
edertelhado <snekoyoru@gmail.com>
2024-06-04 22:51:48 -0300

    INITIAL COMMIT

---
+++ src/test/java/info/capybaratech/capydent/CapydentApplicationTests.java
@@ -0,0 +1,13 @@
+package info.capybaratech.capydent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapydentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
